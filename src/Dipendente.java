38bb7b8e6ecb044f90224143627b9414e25f3618
OttPFC <ale.maz097@gmail.com>
2024-04-18 14:20:20 +0200

    commit

--- src/Dipendente.java
+++ src/Dipendente.java
@@ -1,3 +1,18 @@
 public class Dipendente {
 
+    private int matricola;
+    private float stipendio;
+    private String[] dipartimento = new String["PRODUZIONE","AMMINISTRAZIONE","VENDITE"];
+
+    public Dipendente(int matricola, float stipendio) {
+        this.matricola = matricola;
+        this.stipendio = stipendio;
+    }
+
+    public int getMatricola() {
+        return matricola;
+    }
+    public float getStipendio() {
+        return stipendio;
+    }
 }
