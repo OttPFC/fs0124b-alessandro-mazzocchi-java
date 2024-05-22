871ab887e0b80b885ef095538aea8219f2bd0b98
OttPFC <ale.maz097@gmail.com>
2024-05-22 16:01:30 +0200

    esercizio

---
+++ M2-S2-G3/M2-S2-G3/src/main/java/com/esercizio/M2S2G3/M2S2G3Application.java
@@ -0,0 +1,13 @@
+package com.esercizio.M2S2G3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M2S2G3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M2S2G3Application.class, args);
+	}
+
+}
---
+++ M2-S2-G3/M2-S2-G3/src/test/java/com/esercizio/M2S2G3/M2S2G3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esercizio.M2S2G3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M2S2G3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
